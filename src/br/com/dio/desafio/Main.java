88d85bc3ad26701fbdd24558b02bcfc439530912
Karen Ferraz <101227284+Karenkfr@users.noreply.github.com>
2023-03-14 14:29:05 -0300

    Update Main.java

--- src/br/com/dio/desafio/Main.java
+++ src/br/com/dio/desafio/Main.java
@@ -15,7 +15,9 @@ Curso curso1 = new Curso();
 		
 		curso1.setTitulo("curso Java");
 		curso1.setDescricao("descrição curso Java");
+		System.out.println(curso1.mostraEvolucao());
 		curso1.setCargaHoraria(8);
+		System.out.println(curso1.mostraEvolucao());
 		
 		
 		Curso curso2 = new Curso();
@@ -25,13 +27,15 @@ Curso curso1 = new Curso();
 		curso2.setCargaHoraria(4);
 		
 		
-		
-		
+	
 		
 		Mentoria mentoria = new Mentoria();
+		System.out.println(mentoria.mostraEvolucao());
 		mentoria.setTitulo("mentoria Java");
+		System.out.println(mentoria.mostraEvolucao());
 		mentoria.setDescricao("descrição mentoria Java");
 		mentoria.setData(LocalDate.now());
+		System.out.println(mentoria.mostraEvolucao());
 		
 //		System.out.println(curso1);
 //		System.out.println(mentoria);
@@ -39,10 +43,11 @@ Curso curso1 = new Curso();
 		
 		Bootcamp bootcamp = new Bootcamp();
 		bootcamp.setNome("Bootcamp Java Developer");
-		bootcamp.setDescricao("Descrição Bootcamp Jva Developer");
+		bootcamp.setDescricao("Descrição Bootcamp Java Developer");
 		bootcamp.getConteudos().add(curso1);
 		bootcamp.getConteudos().add(curso2);
 		bootcamp.getConteudos().add(mentoria);
+		System.out.println(bootcamp.mostraEvolucao());
 
 		Dev devCamila = new Dev();
 		devCamila.setNome("Camila");
@@ -61,10 +66,14 @@ Curso curso1 = new Curso();
 		Dev devJoao = new Dev();
 		devJoao.setNome("João");
 		devJoao.inscreverBootcamp(bootcamp);
-		System.out.println("Conteudos Inscritos" + devCamila.getConteudosInscritos());
+		System.out.println("Conteudos Inscritos João" + devJoao.getConteudosInscritos());
+		System.out.println("XP:" + devJoao.calcularXp());
+		System.out.println(devJoao.mostraEvolucao());
 		devJoao.progredir();
 		devJoao.progredir();
 		devJoao.progredir();
+		System.out.println("XP:" + devJoao.calcularXp());
+		System.out.println(devJoao.mostraEvolucao());
 		
 		System.out.println("-");
 		System.out.println("Conteúdos inscritos João" + devJoao.getConteudosInscritos());
